3a9e3deacc48ce683225b989d6743d1376be51ed
Jhonatan Nunes da Silva <jhonatannunessilva@hotmail.com>
2021-07-06 20:41:10 -0300

    added a new solution to the secret handshake exercise

--- secret-handshake/lib/secret_handshake.ex
+++ secret-handshake/lib/secret_handshake.ex
@@ -1,6 +1,4 @@
 defmodule SecretHandshake do
-  use Bitwise
-
   @doc """
   Determine the actions of a secret handshake based on the binary
   representation of the given `code`.
@@ -16,35 +14,52 @@ defmodule SecretHandshake do
   10000 = Reverse the order of the operations in the secret handshake
   """
 
-  @secret %{
-    1 => "wink",
-    2 => "double blink",
-    4 => "close your eyes",
-    8 => "jump"
-  }
+  # use Bitwise
 
   @spec commands(code :: integer) :: list(String.t())
+  def commands(code) do
+    code
+    |> Integer.digits(2)
+    |> Enum.reverse()
+    |> Enum.with_index()
+    |> Enum.reduce([], &add_text/2)
+    |> Enum.reverse()
+  end
+
+  defp add_text({0, _}, acc), do: acc
+  defp add_text({_, 0}, acc), do: ["wink" | acc]
+  defp add_text({_, 1}, acc), do: ["double blink" | acc]
+  defp add_text({_, 2}, acc), do: ["close your eyes" | acc]
+  defp add_text({_, 3}, acc), do: ["jump" | acc]
+  defp add_text({_, 4}, acc), do: Enum.reverse(acc)
+  defp add_text({_, 5}, acc), do: acc
+
+  # @secret %{
+  #   1 => "wink",
+  #   2 => "double blink",
+  #   4 => "close your eyes",
+  #   8 => "jump"
+  # }
+
+  # def commands(code) do
+  #   response = Enum.reduce(@secret, [], fn { key, value }, acc ->
+  #     if (code &&& key) > 0 do
+  #       [value | acc]
+  #     else
+  #       acc
+  #     end
+  #   end)
+
+  #   if (code &&& 16) == 0 do
+  #     Enum.reverse(response)
+  #   else
+  #     response
+  #   end
+  # end
+
   # def commands(code), do: commands(code, [])
   # def commands(code, acc) when (code &&& 1) > 0, do: [@secret[1] | acc]
   # def commands(code, acc) when (code &&& 2) > 0, do: [@secret[2] | acc]
   # def commands(code, acc) when (code &&& 4) > 0, do: [@secret[4] | acc]
   # def commands(code, acc) when (code &&& 8) > 0, do: [@secret[8] | acc]
-
-  def commands(code) do
-    # Integer.digits(code, 2) |> Enum.reverse |> Enum.with_index
-
-    response = Enum.reduce(@secret, [], fn { key, value }, acc ->
-      if (code &&& key) > 0 do
-        [value | acc]
-      else
-        acc
-      end 
-    end)
-
-    if (code &&& 16) == 0 do
-      Enum.reverse(response)
-    else
-      response
-    end
-  end
 end
